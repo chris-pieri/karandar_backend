867c896c7e05bfc4900ecdd4eeccc1c0bf725200
Chris Pieri <chrispieri3@gmail.com>
2023-12-07 17:17:55 -0500

    Testing deno deploy

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import { Context, Hono } from "npm:hono";
 import { cors } from "npm:hono/cors";
-import { MongoClient } from "npm:mongodb";
+// import { MongoClient } from "npm:mongodb";
 
-const connectionString = Deno.env.get("MONGO_CONNECTION")!;
+// const connectionString = Deno.env.get("MONGO_CONNECTION")!;
 
 // Connect to MongoDB
-const client = new MongoClient(connectionString);
-const db = client.db("karandar");
+// const client = new MongoClient(connectionString);
+// const db = client.db("karandar");
 
 const app = new Hono();
 
@@ -19,30 +19,31 @@ interface IDate {
 app.use("*", cors());
 
 app.get("/", async (c: Context) => {
-  const movies = db.collection("movies");
+  return c.text("testing");
+  // const movies = db.collection("movies");
 
-  const query = { title: "Back to the Future" };
-  const movie = await movies.findOne(query);
-  return c.json(movie);
+  // const query = { title: "Back to the Future" };
+  // const movie = await movies.findOne(query);
+  // return c.json(movie);
 });
 
 app.options("*", (c: Context) => {
   return c.text("", 204);
 });
 
-app.get("/dates", async (c: Context) => {
-  const cursor = await db.collection<IDate>("dates").find();
-  const dates = await cursor.toArray();
-  return c.json(dates as any);
-});
+// app.get("/dates", async (c: Context) => {
+//   const cursor = await db.collection<IDate>("dates").find();
+//   const dates = await cursor.toArray();
+//   return c.json(dates as any);
+// });
 
-app.post("/date", async (c: Context) => {
-  const body = await c.req.json();
-  console.log("body", body);
-  const dates = db.collection("dates");
+// app.post("/date", async (c: Context) => {
+//   const body = await c.req.json();
+//   console.log("body", body);
+//   const dates = db.collection("dates");
 
-  const date = await dates.insertOne(body);
-  return c.json({ date });
-});
+//   const date = await dates.insertOne(body);
+//   return c.json({ date });
+// });
 
 Deno.serve({ port: 3000 }, app.fetch);
