0fd195721911a3dc434b2c7ccd8fc30a9c2242b6
Chris Pieri <chrispieri3@gmail.com>
2023-12-07 16:52:57 -0500

    Added Mongo and Hono

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,48 @@
-import { Context, Hono } from "https://deno.land/x/hono@v3.11.2/mod.ts";
+import { Context, Hono } from "npm:hono";
+import { cors } from "npm:hono/cors";
+import { MongoClient } from "npm:mongodb";
+
+const connectionString = Deno.env.get("DENO_MONGO_CONNECTION")!;
+
+// Connect to MongoDB
+const client = new MongoClient(connectionString);
+const db = client.db("karandar");
 
 const app = new Hono();
 
-app.get("/", (c: Context) => c.text("Hello Deno!"));
+interface IDate {
+  title: string;
+  description: string;
+  date: Date;
+}
+
+app.use("*", cors());
+
+app.get("/", async (c: Context) => {
+  const movies = db.collection("movies");
+
+  const query = { title: "Back to the Future" };
+  const movie = await movies.findOne(query);
+  return c.json(movie);
+});
+
+app.options("*", (c: Context) => {
+  return c.text("", 204);
+});
+
+app.get("/dates", async (c: Context) => {
+  const cursor = await db.collection<IDate>("dates").find();
+  const dates = await cursor.toArray();
+  return c.json(dates as any);
+});
+
+app.post("/date", async (c: Context) => {
+  const body = await c.req.json();
+  console.log("body", body);
+  const dates = db.collection("dates");
+
+  const date = await dates.insertOne(body);
+  return c.json({ date });
+});
 
 Deno.serve({ port: 3000 }, app.fetch);
