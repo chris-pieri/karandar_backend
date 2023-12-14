a4d5a1e1f2748b162dd852ae16aef15eec670a2e
Chris Pieri <chrispieri3@gmail.com>
2023-12-14 18:11:25 -0500

    Add support for images

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
 import { Context, Hono } from "npm:hono";
 import { cors } from "npm:hono/cors";
 import { MongoClient, ObjectId } from "npm:mongodb";
+import {
+  GetObjectCommand,
+  PutObjectCommand,
+  S3Client,
+} from "npm:@aws-sdk/client-s3";
+import { getSignedUrl } from "npm:@aws-sdk/s3-request-presigner";
 
-const connectionString = Deno.env.get("MONGO_CONNECTION")!;
+const BUCKET_NAME = Deno.env.get("BUCKET_NAME")!;
+const BUCKET_REGION = Deno.env.get("BUCKET_REGION")!;
+const AWS_ACCESS_KEY = Deno.env.get("AWS_ACCESS_KEY")!;
+const AWS_SECRET_KEY = Deno.env.get("AWS_SECRET_KEY")!;
+
+const s3 = new S3Client({
+  credentials: {
+    accessKeyId: AWS_ACCESS_KEY,
+    secretAccessKey: AWS_SECRET_KEY,
+  },
+  region: BUCKET_REGION,
+});
+
+const CONNECTION_STRING = Deno.env.get("MONGO_CONNECTION")!;
 
 // Connect to MongoDB
-const client = new MongoClient(connectionString);
+const client = new MongoClient(CONNECTION_STRING);
 const db = client.db("karandar");
 
 const app = new Hono();
@@ -14,17 +33,11 @@ interface IDate {
   title: string;
   description: string;
   date: Date;
+  images: string[];
+  imageUrls: string[];
 }
 
 app.use("*", cors());
-
-app.get("/", async (c: Context) => {
-  const movies = db.collection("movies");
-
-  const query = { title: "Back to the Future" };
-  const movie = await movies.findOne(query);
-  return c.json(movie);
-});
 
 app.options("*", (c: Context) => {
   return c.text("", 204);
@@ -33,6 +46,20 @@ app.options("*", (c: Context) => {
 app.get("/dates", async (c: Context) => {
   const cursor = await db.collection<IDate>("dates").find();
   const dates = await cursor.toArray();
+
+  for (const date of dates) {
+    date.imageUrls = [];
+    for (const image of date?.images || []) {
+      const params = {
+        Bucket: BUCKET_NAME,
+        Key: image,
+      };
+      const command = new GetObjectCommand(params);
+      const url = await getSignedUrl(s3, command, { expiresIn: 3600 });
+      date.imageUrls.push(url);
+    }
+  }
+
   return c.json(dates as any);
 });
 
@@ -46,9 +73,32 @@ app.post("/date", async (c: Context) => {
 
 app.put("/date/:id", async (c: Context) => {
   const id = c.req.param("id");
-  const body = await c.req.json();
-  const dates = db.collection("dates");
+  const body = await c.req.parseBody({ all: true });
 
+  const images = body["images"] as File[];
+  const imageLocations = [];
+
+  // only supports multiple files
+
+  for (const image of images) {
+    const buffer = await image.arrayBuffer();
+    const key = `${id}/${image.name}`;
+    const params = {
+      Bucket: BUCKET_NAME,
+      Key: key,
+      Body: buffer,
+      ContentType: image.type,
+    };
+
+    const command = new PutObjectCommand(params);
+
+    await s3.send(command);
+
+    imageLocations.push(key);
+  }
+
+  const dates = db.collection("dates");
+  body["images"] = imageLocations;
   const date = await dates.replaceOne({ _id: new ObjectId(id) }, body);
   return c.json({ date });
 });
